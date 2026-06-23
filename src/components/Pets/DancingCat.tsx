7a3d0fa12b4ad3039a924cad845867c4715b494e
Isai Reyes <anonymus1201a@gmail.com>
2026-06-22 21:46:59 -0600

    aCTUALIZACION

--- src/components/Pets/DancingCat.tsx
+++ src/components/Pets/DancingCat.tsx
@@ -18,7 +18,7 @@ export default function DancingCat({ visible }: Props) {
       <h2>GATO TEST</h2>
 
       <img
-        src="200.gif"
+        src="ss.gif"
         alt="cat"
         style={{
           width: 250,
